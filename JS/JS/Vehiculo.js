6ef908fa11237862386e7aa72046c5b8f09b7a8f
salvarezb@uniremingtonmanizales.edu.co <salvarezb@uniremingtonmanizales.edu.co>
2016-11-21 12:28:14 -0500

    java

--- JS/JS/Vehiculo.js
+++ JS/JS/Vehiculo.js
@@ -5,45 +5,58 @@
 	var emodelo=document.getElementById('emodelo');
 	var eimagen=document.getElementById('eimagen');
 	var ecilindraje=document.getElementById('ecilindraje');
+
+	var iniciar=document.getElementById('ini');
+	var pausar=document.getElementById('pau');
+	var detener=document.getElementById('det');
+	var avanzar=document.getElementById('ava');
+	var izquierda=document.getElementById('izq');
+	var derecha=document.getElementById('der');
 	
+	var vhcl=document.getElementById('vhcl')
 
 	var Vehiculo = {
 		//atributos
 		marca:'Volkswagen',
 		referncia:'Beatle',
 		modelo: 2017,
-		imagen: 'imgs/vw.jpg',
+		imagen: 'imgs/vw.png',
 		cilindrajre:2000,
 		//metodos
 
 		info:function(){
-			emarca.innerHTML = this.marca;
-			ereferencia.innerHTML = this.referncia;
-			emodelo.innerHTML = this.modelo;
-			eimagen.innerHTML = '<img src='+this.imagen+'>';
-			ecilindraje.innerHTML = this.cilindrajre;
+			emarca.innerHTML = '<span>Marca</span>'+this.marca;
+			ereferencia.innerHTML = '<span>Referencia</span>'+this.referncia;
+			emodelo.innerHTML = '<span>Modelo</span>'+this.modelo;
+			eimagen.innerHTML = '<span>Imagen</span>'+'<img src='+this.imagen+'>';
+			ecilindraje.innerHTML = '<span>Cilindraje</span>'+this.cilindrajre;
 
 		},
-		arrancar:function(){
+		
+		controles:function(){
+			iniciar.onclick=function(){
+					vhcl.classList.add('arrancar');
+			}
+			pausar.onclick=function(){
+					vhcl.classList.remove('arrancar');
+					vhcl.classList.remove('retroceso');
+					vhcl.classList.remove('izquierda');
+					vhcl.classList.remove('avanzar');
+					vhcl.classList.remove('derecha');
+			}
+			detener.onclick=function(){
+					vhcl.classList.add('retroceso');
 
-		},
-		apagar:function(){
-
-		},
-		adelantar:function(){
-
-		},
-		frenar:function(){
-
-		},
-		girar_d:function(){
-
-		},
-		girar_i:function(){
-
-		},
-		retroceder:function(){
-
+			}
+			avanzar.onclick=function(){
+					vhcl.classList.toggle('avanzar');
+			}
+			izquierda.onclick=function(){
+					vhcl.classList.toggle('izquierda');		
+			}
+			derecha.onclick=function(){
+					vhcl.classList.toggle('derecha');
+			}
 		}
 
 
